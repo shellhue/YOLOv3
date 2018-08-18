eaa2b1c440b24a6720d6a95541e37ea8e5da50a5
shellhue <zeyu_huang@163.com>
2018-08-18 20:18:55 +0800

    change batch size

--- model/model.py
+++ model/model.py
@@ -158,7 +158,7 @@ def train(annotation_path, classes_path, anchors_path, weights_path, log_dir):
     np.random.shuffle(annotations)
     num_val = int(len(annotations) * val_split)
     num_train = len(annotations) - num_val
-    batch_size = 32
+    batch_size = 8
 
     # first just train the three output layer
     if True:
