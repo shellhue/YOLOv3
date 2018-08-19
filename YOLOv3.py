b188f131284fe04018f7cee8a52a94d5a4b39349
shellhue <zeyu_huang@163.com>
2018-08-19 18:53:28 +0800

    reshape train logic

--- YOLOv3.py
+++ YOLOv3.py
@@ -1,52 +1,61 @@
 import numpy as np
+import argparse
 import colorsys
 from timeit import default_timer as timer
 from PIL import Image, ImageFont, ImageDraw
 
 import keras.backend as K
 from keras.layers import Input
-from keras.models import load_model
+from keras.optimizers import Adam
+from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 
-from model.model import get_detected_boxes, base_model
-from model.dataset import get_anchors, get_classes
+from model.model import get_detected_boxes, base_model, training_model
 from model.utils import resize_image
+from model.dataset import get_classes, get_anchors, data_generator
 
-import tensorflow as tf
+
+parser = argparse.ArgumentParser(description='Darknet To Keras Converter.')
+parser.add_argument('use_retrained_model', help='Whether should use retrained model weights')
+parser.add_argument('annotations_path', help='Path to annotations.')
 
 
 class YOLOv3(object):
     def __init__(self,
-                 model_path="model_data/yolo.h5",
-                 anchor_path="model_data/yolo_anchors.txt",
+                 pretrained_weights_path='model_data/yolo.h5',
+                 retrained_weights_path='model_data/log/trained_weights_stage_final.h5',
+                 anchors_path="model_data/yolo_anchors.txt",
                  classes_path="model_data/coco_classes.txt",
+                 annotations_path='/home/shellhue/data/train.txt',
+                 log_dir='model_data/log/',
+                 use_retrained_weights=False,
                  score_threshold=0.7,
                  iou_threshold=0.45,
                  input_shape=(416, 416)):
-        self.model_path = model_path
-        self.anchor_path = anchor_path
+        self.pretrained_weights_path = pretrained_weights_path
+        self.retrained_weights_path = retrained_weights_path
+        self.anchors_path = anchors_path
         self.classes_path = classes_path
+        self.annotations_path = annotations_path
+        self.log_dir = log_dir
+        self.use_retrained_weights = use_retrained_weights
 
         self.score_threshold = score_threshold
         self.iou_threshold = iou_threshold
         self.input_shape = input_shape
         self.image_shape_placeholder = K.placeholder((2,), dtype='int32')
         self.image_input = Input((None, None, 3), dtype='float32')
-        self.anchors = get_anchors(self.anchor_path)
+        self.anchors = get_anchors(self.anchors_path)
         self.max_boxes = 20
         self.class_names = get_classes(self.classes_path)
         self.sess = K.get_session()
 
-        # load model
-        try:
-            self.model = load_model(self.model_path, compile=False)
-        except:
-            self.model = base_model(self.image_input)
-            self.model.load_weights(self.model_path)
-        else:
-            assert self.model.layers[-1].output_shape[-1] == \
-                len(self.anchors) / len(self.model.outputs) * (len(self.class_names) + 5), \
-                'Mismatch between model and given anchor and class sizes'
-        weights = self.model.get_weights()
+        self.model = base_model(self.image_input)
+        # load model weights
+        self.model.load_weights(self.retrained_weights_path if self.use_retrained_weights else self.pretrained_weights_path)
+        assert self.model.layers[-1].output_shape[-1] == \
+            len(self.anchors) / len(self.model.outputs) * (len(self.class_names) + 5), \
+            'Mismatch between model and given anchor and class sizes'
+
         # get detected boxes
         self.boxes, self.scores, self.classes = get_detected_boxes(self.model.outputs,
                                                                    self.image_shape_placeholder,
@@ -63,6 +72,72 @@ class YOLOv3(object):
         np.random.seed(23132)
         np.random.shuffle(self.colors)
         np.random.seed(None)
+
+    def train(self):
+        input_shape = (416, 416)
+        class_names = get_classes(self.classes_path)
+        anchors = get_anchors(self.anchors_path)
+        num_classes = len(class_names)
+        model = training_model(input_shape, anchors,
+                               num_classes=len(class_names),
+                               weights_path=self.pretrained_weights_path,
+                               freeze_body=2)
+
+        logging = TensorBoard(self.log_dir)
+        checkpoint = ModelCheckpoint(self.log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
+                                     monitor='val_loss',
+                                     save_weights_only=True,
+                                     save_best_only=True,
+                                     period=3)
+        reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=3, verbose=1)
+        early_stopping = EarlyStopping('val_loss', min_delta=0, patience=10, verbose=1)
+
+        val_split = 0.1
+        with open(self.annotations_path) as f:
+            annotations = f.readlines()
+        np.random.seed(4)
+        np.random.shuffle(annotations)
+        num_val = int(len(annotations) // 1000 * val_split)
+        num_train = len(annotations) // 1000 - num_val
+        batch_size = 8
+
+        # first just train the three output layer
+        if True:
+            model.compile(
+                Adam(lr=1e-3),
+                loss={
+                    'yolo_loss': lambda label, pred: pred
+                })
+            model.fit_generator(
+                data_generator(annotations[:num_train], input_shape, batch_size, anchors, num_classes),
+                steps_per_epoch=max(1, num_train // batch_size),
+                epochs=50,
+                validation_data=data_generator(annotations[num_train:], input_shape, batch_size, anchors, num_classes),
+                validation_steps=max(1, num_val // batch_size),
+                initial_epoch=0,
+                callbacks=[logging, checkpoint]
+            )
+            model.save_weights(self.log_dir + 'trained_weights_stage_1.h5')
+
+        # fine tuning all layers
+        if True:
+            for l in range(len(model.layers)):
+                model.layers[l].trainable = True
+            model.compile(
+                Adam(lr=1e-4),
+                loss={
+                    'yolo_loss': lambda label, pred: pred
+                })
+            model.fit_generator(
+                data_generator(annotations[:num_train], input_shape, batch_size, anchors, num_classes),
+                steps_per_epoch=max(1, num_train // batch_size),
+                epochs=100,
+                validation_data=data_generator(annotations[num_train:], input_shape, batch_size, anchors, num_classes),
+                validation_steps=max(1, num_val // batch_size),
+                initial_epoch=50,
+                callbacks=[logging, checkpoint, reduce_lr, early_stopping]
+            )
+            model.save_weights(self.log_dir + 'trained_weights_stage_final.h5')
 
     def detect_image(self, image):
         start = timer()
@@ -118,10 +193,11 @@ class YOLOv3(object):
         self.sess.close()
 
 
-tensor = tf.constant([100, 22])
-tensorb = tensor * 2
+def _main(args):
+    yolo = YOLOv3(annotations_path=args.annotations_path, use_retrained_weights=args.use_retrained_weights)
+    yolo.train()
 
-tensor = tf.Print(tensor, [tensor, tensorb, tf.constant(100)], message='tensor print test: ')
 
-with tf.Session() as sess:
-    print(sess.run([tensor, tensor]))
+if __name__ == '__main__':
+
+    _main(parser.parse_args())
--- model/model.py
+++ model/model.py
@@ -10,11 +10,8 @@ from keras.layers import Lambda
 from keras.layers import Add
 from keras.layers.advanced_activations import LeakyReLU
 from keras.regularizers import l2
-from keras.optimizers import Adam
-from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 
 from model.utils import squeeze
-from model.dataset import get_classes, get_anchors, data_generator
 
 
 def DarknetConv2D(*args, **kwargs):
@@ -122,81 +119,6 @@ def base_model(image_input):
     x, y3 = predict(feature3, 128, x, up_sampling=True)
 
     return Model(image_input, [y1, y2, y3])
-
-
-def train(annotation_path, classes_path, anchors_path, weights_path, log_dir):
-    """Train YOLOv3 model
-
-    annotation_path: string
-    classes_path: string
-    anchors_path: string
-    weights_path: string
-    log_dir: string
-    """
-    input_shape = (416, 416)
-    class_names = get_classes(classes_path)
-    anchors = get_anchors(anchors_path)
-    num_classes = len(class_names)
-    model = training_model(input_shape, anchors,
-                           num_classes=len(class_names),
-                           weights_path=weights_path,
-                           freeze_body=2)
-
-    logging = TensorBoard(log_dir)
-    checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
-                                 monitor='val_loss',
-                                 save_weights_only=True,
-                                 save_best_only=True,
-                                 period=3)
-    reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=3, verbose=1)
-    early_stopping = EarlyStopping('val_loss', min_delta=0, patience=10, verbose=1)
-
-    val_split = 0.1
-    with open(annotation_path) as f:
-        annotations = f.readlines()
-    np.random.seed(4)
-    np.random.shuffle(annotations)
-    num_val = int(len(annotations) // 1000 * val_split)
-    num_train = len(annotations) // 1000 - num_val
-    batch_size = 8
-
-    # first just train the three output layer
-    if True:
-        model.compile(
-            Adam(lr=1e-3),
-            loss={
-                'yolo_loss': lambda label, pred: pred
-            })
-        model.fit_generator(
-            data_generator(annotations[:num_train], input_shape, batch_size, anchors, num_classes),
-            steps_per_epoch=max(1, num_train // batch_size),
-            epochs=50,
-            validation_data=data_generator(annotations[num_train:], input_shape, batch_size, anchors, num_classes),
-            validation_steps=max(1, num_val // batch_size),
-            initial_epoch=0,
-            callbacks=[logging, checkpoint]
-        )
-        model.save_weights(log_dir + 'trained_weights_stage_1.h5')
-
-    # fine tuning all layers
-    if True:
-        for l in range(len(model.layers)):
-            model.layers[l].trainable = True
-        model.compile(
-            Adam(lr=1e-4),
-            loss={
-                'yolo_loss': lambda label, pred: pred
-            })
-        model.fit_generator(
-            data_generator(annotations[:num_train], input_shape, batch_size, anchors, num_classes),
-            steps_per_epoch=max(1, num_train // batch_size),
-            epochs=100,
-            validation_data=data_generator(annotations[num_train:], input_shape, batch_size, anchors, num_classes),
-            validation_steps=max(1, num_val // batch_size),
-            initial_epoch=50,
-            callbacks=[logging, checkpoint, reduce_lr, early_stopping]
-        )
-        model.save_weights(log_dir + 'trained_weights_stage_final.h5')
 
 
 def preprocess_pred(pred, input_shape, anchors, num_classes):
